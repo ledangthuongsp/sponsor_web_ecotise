ffd31cf207a12998a707d8531c1e7205f7987d02
IDS-V-Thuongld <thuongld@vn.ids.jp>
2025-05-23 00:26:04 +0700

    Sửa log

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Chỉ định thư mục gốc cho Vite
+  root: '.',
   resolve: {
       alias: {
-          '@': '/src', // Alias cho thư mục src
+          '@': '/src',
       },
   },
+  base: '/sponsor_web_ecotise/',
 })
