a61287f5871eab2385b6dfe5a0d597b48c0b8984
IDS-V-Thuongld <thuongld@vn.ids.jp>
2025-07-11 01:11:54 +0700

    fix

--- src/constants/api_config.jsx
+++ src/constants/api_config.jsx
@@ -1,7 +1,7 @@
 const API_CONFIG = {
   // BASE_URL: 'https://ecots-be.onrender.com/',
-  // BASE_URL: 'https://ecots-be.onrender.com'
-  BASE_URL: 'http://localhost:7050',
+  BASE_URL: 'https://ecots-be.onrender.com'
+  // BASE_URL: 'http://localhost:7050',
 };
 
 export default API_CONFIG;
